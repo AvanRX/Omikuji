96ab1fb1aaf38c55e3cd661f89f53412097f0359
yamamiya <touyama07@gmail.com>
2020-07-06 14:15:02 +0900

    oo

--- server.js
+++ server.js
@@ -1,59 +1,77 @@
 const express=require("express");
 const app=express();
 const port=3000;
-const fortune=["大吉","中吉","小吉","吉","末吉","凶","大凶"];
+const fort=["大吉","中吉","小吉","吉","末吉","凶","大凶"];
 
-app.get("/",(req,res)=>{
+app.get("/:form",(req,res)=>{
     
     const fs=require("fs");
+    var q=req.query.form;
+    var data;//出力用データ
+    var rand=Math.floor(Math.random()*Math.floor(fort.length+1))
 
-    var query=req.query;
-    var data={};//出力用データ
-    var rand=Math.floor(Math.random()*Math.floor(fortune.length+1))
-    if(query=="omikuji"){
-        data={
-            "fortune":fortune[rand]
-        }
-
-        var logs=null;
+    if(q=="omikuji"){
+    
+        var logs="null";
+        var logJson;
         try{
-            logs=fs.readFile("public/histry.txt");
-            logs=logs.json();
+            var fd=fs.readFileSync("public/histry.txt");
+            logJson=JSON.stringify(fd.toString());
         }
         catch(e){
             console.log(e.message);
         }
-
+        
         //過去履歴がなかった場合新規に作る
-        if(logs==null){
-            for(let i=0;i<fortune.length;i++){
-                logs+="\""+fortune[i]+"\""+":"+"0";//ヒストリjsonの初期化
+        if(logs=="null"){
+            var str;
+            str+="{";
+            for(let i=0;i<fort.length;i++){
+                str+="\""+fort[i]+"\""+": 0";//ヒストリjsonの初期化
             }
-            logs=logs.json();
+            str+="}";
+            logs=str;
+            logJson=JSON.stringify(str);
         }
-        console.log(logs.json());
 
-        logs[fortune[rand]]+=1;
-
-        fs.writeFile("public/histry.txt",logs);
+        logJson=JSON.parse(logs);
+        
+       // console.log(logs);
+        logJson.fortune+=1;
+        
+        fs.writeFile("public/histry.txt",toString(logs),(err)=>{
+            if(err){
+                console.log(`error${err}`);
+            }
+        });
 
         data={
-            "fortune":fortune[rand]
+            fortune: fort[rand]
         }
+        //console.log(data.fortune);
     }
-    if(query=="analytics"){
+    if(q=="analytics"){
         var logs=null;
         try{
             logs=fs.readFile("public/histry.txt");
-            logs=logs.json();
+            logs=JSON.stringify(logs.toString());
         }
-        catch(e){
-            console.log(e.message);
+        catch(e){ 
+            //過去履歴がなかった場合新規に作る
+            if(logs==null){
+                var str;
+                str+="{";
+                for(let i=0;i<fort.length;i++){
+                    str+="\""+fort[i]+"\""+": 0";//ヒストリjsonの初期化
+                }
+                str+="}";
+                logs=JSON.stringify(str);
+            }
+            //console.log(e.message);
         }
-
-        data.jso(logs);
     }
 
+    console.log(data);
     res.json(data);
 });
 
